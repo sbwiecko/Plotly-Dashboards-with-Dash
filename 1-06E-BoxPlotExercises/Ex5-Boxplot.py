778e2a156cb2b5970d735c9bf1f17269ba1766ec
Sebastien Wieckowski <sbwiecko@free.fr>
2020-09-04 16:12:27 +0200

    boxplot part completed

--- 1-06E-BoxPlotExercises/Ex5-Boxplot.py
+++ 1-06E-BoxPlotExercises/Ex5-Boxplot.py
@@ -4,35 +4,31 @@
 # HINT: np.random.choice(df['rings'],10,replace=False) takes 10 random values
 # Use box plots to show that the samples do derive from the same population.
 ######
-
+# %%
 # Perform imports here:
-
-
-
-
+from os import replace
+import pandas as pd
+import plotly.graph_objs as go
+import plotly.offline as pyo
 
 # create a DataFrame from the .csv file:
-
-
+df = pd.read_csv('../Data/abalone.csv')
+# %%
 # take two random samples of different sizes:
-
-
+sample_1 = df['rings'].sample(n=100, replace=True)
+sample_2 = df['rings'].sample(n=100, replace=True)
 
 # create a data variable with two Box plots:
-
-
-
-
-
-
-
-
-
-
+data = [go.Box(y=sample_1,
+               name='Sample 1'),
+        go.Box(y=sample_2,
+               name='Sample 2')]
 
 # add a layout
-
-
-
+layout = go.Layout(
+    title = 'Comparison of the distribution of 10-samples from the same population'
+)
 
 # create a fig from data & layout, and plot the fig
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig, filename='box-1-06E.html')
