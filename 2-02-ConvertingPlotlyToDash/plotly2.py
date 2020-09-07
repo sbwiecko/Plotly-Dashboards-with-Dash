7d20bfeb74a396fa477b11f1427aa0f92cb1def1
sbwiecko <sbwiecko@free.fr>
2020-09-07 12:34:05 +0200

    clean structure code multipanel dashboard

--- 2-02-ConvertingPlotlyToDash/plotly2.py
+++ 2-02-ConvertingPlotlyToDash/plotly2.py
@@ -11,38 +11,56 @@ np.random.seed(42)
 random_x = np.random.randint(1,101,100)
 random_y = np.random.randint(1,101,100)
 
-app.layout = html.Div([dcc.Graph(id='scatterplot',
-                    figure = {'data':[
-                            go.Scatter(
-                            x=random_x,
-                            y=random_y,
-                            mode='markers',
-                            marker = {
-                                'size':12,
-                                'color': 'rgb(51,204,153)',
-                                'symbol':'pentagon',
-                                'line':{'width':2}
+app.layout = html.Div(
+    children=[
+        dcc.Graph(
+            id='scatterplot',
+            figure = {
+                'data':[
+                    go.Scatter(
+                        x=random_x,
+                        y=random_y,
+                        mode='markers',
+                        marker = {
+                            'size':12,
+                            'color': 'rgb(51,204,153)',
+                            'symbol':'pentagon',
+                            'line':{'width':2}
                             }
-                            )],
-                    'layout':go.Layout(title='My Scatterplot',
-                                        xaxis = {'title':'Some X title'})}
-                    ),
-                    dcc.Graph(id='scatterplot2',
-                                        figure = {'data':[
-                                                go.Scatter(
-                                                x=random_x,
-                                                y=random_y,
-                                                mode='markers',
-                                                marker = {
-                                                    'size':12,
-                                                    'color': 'rgb(200,204,53)',
-                                                    'symbol':'pentagon',
-                                                    'line':{'width':2}
-                                                }
-                                                )],
-                                        'layout':go.Layout(title='Second Plot',
-                                                            xaxis = {'title':'Some X title'})}
-                                        )])
+                    )
+                ],
+                'layout':go.Layout(
+                    title='My Scatterplot',
+                    xaxis = {
+                        'title':'Some X title'
+                    }
+                )
+            }
+        ),
+        dcc.Graph(
+            id='scatterplot2',
+            figure = {
+                'data':[
+                    go.Scatter(
+                        x=random_x,
+                        y=random_y,
+                        mode='markers',
+                        marker = {
+                            'size':12,
+                            'color': 'rgb(200,204,53)',
+                            'symbol':'pentagon',
+                            'line':{'width':2}
+                        }
+                    )
+                ],
+                'layout':go.Layout(
+                    title='Second Plot',
+                    xaxis = {'title':'Some X title'}
+                )
+            }
+        )
+    ]
+)
 
 if __name__ == '__main__':
     app.run_server()
