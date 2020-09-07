7ce3463792039e74d6ca180a9494b485c7b94530
sbwiecko <sbwiecko@free.fr>
2020-09-07 16:31:24 +0200

    clean structure code 2-05

--- 2-05-DashComponents/HTMLComponents.py
+++ 2-05-DashComponents/HTMLComponents.py
@@ -7,21 +7,37 @@ import dash_html_components as html
 
 app = dash.Dash()
 
-app.layout = html.Div([
-    'This is the outermost Div',
-    html.Div(
-        'This is an inner Div',
-        style={'color':'blue', 'border':'2px blue solid', 'borderRadius':5,
-        'padding':10, 'width':220}
-    ),
-    html.Div(
-        'This is another inner Div',
-        style={'color':'green', 'border':'2px green solid',
-        'margin':10, 'width':220}
-    ),
-],
-# this styles the outermost Div:
-style={'width':500, 'height':200, 'color':'red', 'border':'2px red dotted'})
+app.layout = html.Div(
+    [
+        'This is the outermost Div',
+        html.Div(
+            'This is an inner Div',    # for single item no need for list as a Div children
+            style={
+                'color':'blue',
+                'border':'2px blue solid',
+                'borderRadius':5,
+                'padding':10,
+                'width':100
+            }
+        ),
+        html.Div(
+            'This is another inner Div',
+            style={
+                'color':'green',
+                'border':'4px green solid',
+                'margin':5,
+                'width':100
+            }
+        ),
+    ],
+    # this styles the outermost Div:
+    style={
+        'width':300,
+        'height':100,
+        'color':'red',
+        'border':'2px red dotted'
+    }
+)
 
 if __name__ == '__main__':
     app.run_server()
