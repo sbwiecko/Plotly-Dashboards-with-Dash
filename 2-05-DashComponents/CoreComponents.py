d868bb1b9ec73519b87f922c01b33bf96b26411c
sbwiecko <sbwiecko@free.fr>
2020-09-08 16:19:46 +0200

    started with core components

--- 2-05-DashComponents/CoreComponents.py
+++ 2-05-DashComponents/CoreComponents.py
@@ -8,81 +8,84 @@ import dash_html_components as html
 
 app = dash.Dash()
 
-app.layout = html.Div([
+app.layout = html.Div(
+    [
+        # DROPDOWN https://dash.plot.ly/dash-core-components/dropdown
+        html.Label('Dropdown'),
+        dcc.Dropdown(
+            options=[
+                {
+                    'label': 'New York City',
+                    'value': 'NYC'
+                },
+                {
+                    'label': 'Montréal',
+                    'value': 'MTL'
+                },
+                {
+                    'label': 'San Francisco',
+                    'value': 'SF'
+                }
+            ],
+            value='MTL'
+        ),
 
-    # DROPDOWN https://dash.plot.ly/dash-core-components/dropdown
-    html.Label('Dropdown'),
-    dcc.Dropdown(
-        options=[
-            {
-                'label': 'New York City',
-                'value': 'NYC'
-            },
-            {
-                'label': 'Montréal',
-                'value': 'MTL'
-            },
-            {
-                'label': 'San Francisco',
-                'value': 'SF'
-            }
-        ],
-        value='MTL'
-    ),
+        html.Label('Multi-Select Dropdown'),
+        dcc.Dropdown(
+            options=[
+                {
+                    'label': 'New York City',
+                    'value': 'NYC'
+                },
+                {
+                    'label': u'Montréal',
+                    'value': 'MTL'
+                },
+                {
+                    'label': 'San Francisco',
+                    'value': 'SF'
+                }
+            ],
+            value=['MTL', 'SF'],
+            multi=True
+        ),
 
-    html.Label('Multi-Select Dropdown'),
-    dcc.Dropdown(
-        options=[
-            {
-                'label': 'New York City',
-                'value': 'NYC'
-            },
-            {
-                'label': u'Montréal',
-                'value': 'MTL'
-            },
-            {
-                'label': 'San Francisco',
-                'value': 'SF'
-            }
-        ],
-        value=['MTL', 'SF'],
-        multi=True
-    ),
+        # SLIDER https://dash.plot.ly/dash-core-components/slider
+        html.Label('Slider'),
+        html.P(    # line break
+            dcc.Slider(
+                min=-5,
+                max=10,
+                step=0.5,
+                marks={i: i for i in range(-5,11)},
+                value=-3
+            )
+        ),
 
-    # SLIDER https://dash.plot.ly/dash-core-components/slider
-    html.Label('Slider'),
-    html.P(    # line break
-        dcc.Slider(
-            min=-5,
-            max=10,
-            step=0.5,
-            marks={i: i for i in range(-5,11)},
-            value=-3
+        # RADIO ITEMS https://dash.plot.ly/dash-core-components/radioitems
+        html.Label('Radio Items'),
+        html.P(
+            dcc.RadioItems(
+                options=[
+                    {
+                        'label': 'New York City',
+                        'value': 'NYC'
+                    },
+                    {
+                        'label': 'Montréal',
+                        'value': 'MTL'
+                    },
+                    {
+                        'label': 'San Francisco',
+                        'value': 'SF'
+                    }
+                ],
+                value='MTL'
+            )
         )
-    ),
-
-    # RADIO ITEMS https://dash.plot.ly/dash-core-components/radioitems
-    html.Label('Radio Items'),
-    dcc.RadioItems(
-        options=[
-            {
-                'label': 'New York City',
-                'value': 'NYC'
-            },
-            {
-                'label': 'Montréal',
-                'value': 'MTL'
-            },
-            {
-                'label': 'San Francisco',
-                'value': 'SF'
-            }
-        ],
-        value='MTL'
-    )
-],
-style={'width': '50%'})
+    ],
+    style={'width': '50%'}
+)
 
 if __name__ == '__main__':
     app.run_server()
