ca4e780390ea138dc3b3fa4a1891d7591802c68e
Sebastien Wieckowski <sbwiecko@free.fr>
2020-09-12 15:12:22 +0200

    structure hover1.py

--- 2-13-HoverOverData/hover1.py
+++ 2-13-HoverOverData/hover1.py
@@ -15,40 +15,52 @@ app = dash.Dash()
 df = pd.read_csv('../data/wheels.csv')
 
 app.layout = html.Div([
-    html.Div([
-    dcc.Graph(
-        id='wheels-plot',
-        figure={
-            'data': [
-                go.Scatter(
-                    x = df['color'],
-                    y = df['wheels'],
-                    dy = 1,
-                    mode = 'markers',
-                    marker = {
-                        'size': 12,
-                        'color': 'rgb(51,204,153)',
-                        'line': {'width': 2}
-                        }
-                )
-            ],
-            'layout': go.Layout(
-                title = 'Wheels & Colors Scatterplot',
-                xaxis = {'title': 'Color'},
-                yaxis = {'title': '# of Wheels','nticks':3},
-                hovermode='closest'
+    html.Div(
+        [
+            dcc.Graph(
+                id='wheels-plot',
+                figure={
+                    'data': [
+                        go.Scatter(
+                            x = df['color'],
+                            y = df['wheels'],
+                            dy = 1,
+                            mode = 'markers',
+                            marker = {
+                                'size': 12,
+                                'color': 'rgb(51,204,153)',
+                                'line': {'width': 2}
+                            }
+                        )
+                    ],
+                    'layout': go.Layout(
+                        title = 'Wheels & Colors Scatterplot',
+                        xaxis = {'title': 'Color'},
+                        yaxis = {'title': '# of Wheels','nticks':3},
+                        hovermode='closest'
+                    )
+                }
             )
-        }
-    )], style={'width':'30%', 'float':'left'}),
+        ],
+        style={'width':'30%', 'float':'left'}),
 
-    html.Div([
-    html.Pre(id='hover-data', style={'paddingTop':35})
-    ], style={'width':'30%'})
-])
+        html.Div(
+            [
+                html.Pre(
+                    id='hover-data',         # Text in a <pre> element is displayed in a fixed-width font,
+                    style={'paddingTop':35}  # and the text preserves both spaces and line breaks.
+                )                            # The text will be displayed exactly as written in the HTML source code.
+        ],
+        style={'width':'30%'})
+    ]
+)
 
 @app.callback(
     Output('hover-data', 'children'),
-    [Input('wheels-plot', 'hoverData')])
+    [
+        Input('wheels-plot', 'hoverData')
+    ]
+)
 def callback_image(hoverData):
     return json.dumps(hoverData, indent=2)
 
